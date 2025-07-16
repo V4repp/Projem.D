69eb8487cd61937a4ebcd3d1e702dbf256c04e6f
V4repp <gitph2@gmail.com>
2025-07-16 18:15:15 +0300

    V4repp after edited TestGit class.

--- IdeaProjects/Deneme/src/test/day34/TestGit.java
+++ IdeaProjects/Deneme/src/test/day34/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Deneme 1");
+        System.out.println("değişiklik sonrası, görüntü bu şekilde.");
     }
 }
