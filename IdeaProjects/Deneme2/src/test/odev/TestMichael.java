4f271e30fdf0afdf945023515db6dbe13b06431b
Ersin <michael.knight@cydeo.com>
2025-07-16 18:13:35 +0300

    TestMichael Class is updated

--- IdeaProjects/Deneme2/src/test/odev/TestMichael.java
+++ IdeaProjects/Deneme2/src/test/odev/TestMichael.java
@@ -3,5 +3,6 @@ package test.odev;
 public class TestMichael {
     public static void main(String[] args) {
         System.out.println("Merhabalar Muhammet.");
+        System.out.println("Derslarin nasıl?");
     }
 }
