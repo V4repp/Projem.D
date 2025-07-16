7a1a006cd721373e3ff0462f5b737c2b1c38ef12
Ersin <michael.knight@cydeo.com>
2025-07-16 18:17:09 +0300

    TestMichael Class is updated after lunch

--- IdeaProjects/Deneme2/src/test/odev/TestMichael.java
+++ IdeaProjects/Deneme2/src/test/odev/TestMichael.java
@@ -4,5 +4,6 @@ public class TestMichael {
     public static void main(String[] args) {
         System.out.println("Merhabalar Muhammet.");
         System.out.println("Derslarin nasıl?");
+        System.out.println("Öğle yemeği sonrasında ilk commit.");
     }
 }
