9cdb6f5fc4de917281cbc378243fcb7c421fdae3
V4repp <gitph2@gmail.com>
2025-07-23 16:23:07 +0300

    Created PrintExecution class.

---
+++ IdeaProjects/JavaOdevler/src/FirstHomework/PrintExecution.java
@@ -0,0 +1,19 @@
+package FirstHomework;
+
+public class PrintExecution {
+   /* Create a Java class named PrintExecution that prints the following outputs in the console:
+
+            Cydeo
+
+            Yuliia
+            Alena
+            Lucy
+            Alexander
+
+            Igor
+            Sumeye
+            Madina*/
+    public static void main(String[] args) {
+
+    }
+}
