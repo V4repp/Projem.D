a425e226aded256f93652f76d3a3c4d42b3712fe
V4repp <gitph2@gmail.com>
2025-08-19 15:12:02 +0300

    Edited and added StudentInfo class.

---
+++ IdeaProjects/JavaOdevler/src/SecondHomework/StudentInfo.java
@@ -0,0 +1,37 @@
+package SecondHomework;
+
+public class StudentInfo {/* Create a class named StudentInfo and write a program with the following requirements:
+
+	Declare the following variables with appropriate data types:
+			studentName
+			age
+			gender
+			schoolName
+			studentId
+			gradeLevel
+			gpa
+
+	Display the full student information in the following format:
+
+			Example:
+				studentName = "Josh Steven";
+				age = 22;
+				gender = "Male";
+				gpa = 2.8;
+				schoolName = "MIT";
+				gradeLevel = "Undergraduate";
+				studentId = "A125";
+
+			Output:
+				The student's name is Josh Steven
+				Gender is Male
+				Age is 22 years old
+				The school's name is MIT
+				Grade level is Undergraduate
+				Josh Steven's GPA is 2.8  */
+    public static void main(String[] args) {
+        String studentName = "veli";
+        int age = 16 ;
+
+    }
+}
